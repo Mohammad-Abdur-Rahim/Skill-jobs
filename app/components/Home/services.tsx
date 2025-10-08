734d98215f7f262f6131b34f1436222bdbe829e7
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-10-08 23:34:49 +0600

    Call to action button done

--- app/components/Home/services.tsx
+++ app/components/Home/services.tsx
@@ -4,6 +4,9 @@ import three from "app/assets/3.png";
 import four from "app/assets/4.png";
 import five from "app/assets/5.png";
 import six from "app/assets/6.png";
+import cal from "app/assets/cal.png";
+import call from "app/assets/call.png";
+import map from "app/assets/map.png";
 export default function services() {
   return (
     <>
@@ -137,6 +140,49 @@ export default function services() {
                    More Services
                   </button>
         </div>
+      </section>
+      <section className="w-10/12 mx-auto py-10 ">
+    <div className="bg-black grid grid-cols-1 md:grid-cols-3 py-18 rounded-lg">
+        <div>
+    <div className="flex justify-center items-center gap-8">
+    <div><img src={cal} alt="Calender" /></div>
+    <div>
+          <p className="text-base md:text-lg mt-5 leading-relaxed  text-white mb-4">
+            WE are open monday-friday
+          </p>
+             <h3 className="text-whitemb-2 font-bold text-2xl">7:00 am - 9:00 pm</h3>
+    </div>
+    </div>
+
+        </div>
+
+<div>
+    <div className="flex justify-center items-center gap-8">
+    <div><img src={call} alt="Calender" /></div>
+    <div>
+          <p className="text-base md:text-lg mt-5 leading-relaxed  text-white mb-4">
+         Have a question?
+          </p>
+             <h3 className="text-whitemb-2 font-bold text-2xl">+2546 251 2658</h3>
+    </div>
+    </div>
+
+        </div>
+        <div>
+    <div className="flex justify-center items-center gap-8">
+    <div><img src={map} alt="Calender" /></div>
+    <div>
+          <p className="text-base md:text-lg mt-5 leading-relaxed  text-white mb-4">
+            Need a repair? our address
+          </p>
+             <h3 className="text-whitemb-2 font-bold text-2xl">Liza Street, New York</h3>
+    </div>
+    </div>
+
+        </div>
+
+        
+    </div>
       </section>
     </>
   );
